d821809ef8e70aa3fb37e09306a03257c8c6ab60
Takashi Honda <takashi.0628.honda@gmail.com>
2014-02-15 18:20:13 +0900

    RecomendViewController追加

---
+++ app/TelevisionGuide/TelevisionGuide/RecomendViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecomendViewController.h
+//  TelevisionGuide
+//
+//  Created by Takashi Honda on 2014/02/15.
+//  Copyright (c) 2014年 TEAM TAKOYAKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecomendViewController : UIViewController
+
+@end
---
+++ app/TelevisionGuide/TelevisionGuide/RecomendViewController.m
@@ -0,0 +1,38 @@
+//
+//  RecomendViewController.m
+//  TelevisionGuide
+//
+//  Created by Takashi Honda on 2014/02/15.
+//  Copyright (c) 2014年 TEAM TAKOYAKI. All rights reserved.
+//
+
+#import "RecomendViewController.h"
+
+@interface RecomendViewController ()
+
+@end
+
+@implementation RecomendViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
