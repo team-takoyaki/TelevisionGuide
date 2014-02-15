318a249eb3df98867ab82d9942188807cd7f0177
ctyo <ctyo@live.jp>
2014-02-15 21:00:43 +0900

    add recommend api entry point

---
+++ api/recommend_function.php
@@ -0,0 +1,15 @@
+<?php
+require_once '/var/www/html/api/common.conf';
+
+function loadPgFromJson () {
+    $get_date1 = strftime('%Y-%m-%d', time());
+    $get_date2 = strftime('%Y-%m-%d', strtotime('+1 days',time()));
+
+    $json1 = DATA_DIR.'/pg/'.$get_date1.'.tv.json';
+    $json2 = DATA_DIR.'/pg/'.$get_date2.'.tv.json';
+
+    $data1 = json_decode($json1, true);
+    $data2 = json_decode($json2, true);
+
+    return array_marge( $data1, $data2 );
+}
