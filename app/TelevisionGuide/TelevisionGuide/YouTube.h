29f024a4247da87fbe47eb17d0ba62daff2fee85
Takashi Honda <takashi.0628.honda@gmail.com>
2014-02-16 03:57:41 +0900

    add MusicList

---
+++ app/TelevisionGuide/TelevisionGuide/YouTube.h
@@ -0,0 +1,13 @@
+//
+//  YouTube.h
+//  TelevisionGuide
+//
+//  Created by Takashi Honda on 2014/02/16.
+//  Copyright (c) 2014年 TEAM TAKOYAKI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YouTube : NSObject
++ (void) sendYouTubeHistory;
+@end
---
+++ app/TelevisionGuide/TelevisionGuide/YouTube.m
@@ -0,0 +1,23 @@
+//
+//  YouTube.m
+//  TelevisionGuide
+//
+//  Created by Takashi Honda on 2014/02/16.
+//  Copyright (c) 2014年 TEAM TAKOYAKI. All rights reserved.
+//
+
+#import "YouTube.h"
+
+@interface YouTube()
+
+@end
+
+@implementation YouTube
+
++ (void) sendYouTubeHistory {
+
+
+}
+
+@end
+
